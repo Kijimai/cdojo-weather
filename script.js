f16bb98117327f51d3c0acce7092d4a59573c5a4
John Deen Bucog <JDBucog@yahoo.com>
2022-03-24 16:21:19 -0700

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+//TO DO
+// When to user clicks on a city, display an alert
+// Clicking "I accept" will dismiss the cookie policy message
+// - Make an "active" class to slowly animate it into being, then remove it on a setTimeout by taking the class off.
+// Convert the temperatures when the <select> element is changed
+
+
